81cd5503eae751a4e670811ff9f35d306f905e21
t2314862168 <2314862168@qq.com>
2017-11-20 15:44:25 +0800

    add need view interface

--- app/src/main/java/com/tangxb/demo/ui/activity/AndroidResultActivity.kt
+++ app/src/main/java/com/tangxb/demo/ui/activity/AndroidResultActivity.kt
@@ -3,13 +3,14 @@ package com.tangxb.demo.ui.activity
 import com.tangxb.demo.R
 import com.tangxb.demo.mvp.presenter.AndroidResultPresenter
 import com.tangxb.demo.mvp.presenter.BasePresenter
+import com.tangxb.demo.mvp.view.AndroidResultActivityView
 import com.tangxb.demo.mvp.view.BaseActivityView
 import io.reactivex.functions.Consumer
 
 /**
  * Created by Taxngb on 2017/11/14.
  */
-class AndroidResultActivity : BaseActivity<AndroidResultPresenter>(), BaseActivityView {
+class AndroidResultActivity : BaseActivity<AndroidResultPresenter>(), AndroidResultActivityView {
     var presenter: AndroidResultPresenter? = null
     override fun getLayoutResId(): Int {
         return R.layout.activity_main
